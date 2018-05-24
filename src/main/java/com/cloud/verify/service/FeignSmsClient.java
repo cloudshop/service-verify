9eef325ade2158698b1632a0e5c353f841c67e9b
wgw <756898059@qq.com>
2018-05-24 13:59:51 +0800

    修改fengin调用

--- src/main/java/com/cloud/verify/service/FeignSmsClient.java
+++ src/main/java/com/cloud/verify/service/FeignSmsClient.java
@@ -1,14 +1,15 @@
 package com.cloud.verify.service;
 
-import com.cloud.verify.config.FeignConfiguration;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import com.cloud.verify.client.AuthorizedFeignClient;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Map;
 
-@FeignClient(value = "sms",configuration = FeignConfiguration.class)
+@AuthorizedFeignClient(name = "sms")
 public interface FeignSmsClient {
-    @RequestMapping (value = "/api/messages",method = RequestMethod.POST)
+	
+    @PostMapping (value = "/api/messages")
     public Map SendSmsCode(@RequestBody Map param)throws Exception;
+    
 }
